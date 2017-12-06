4607fed3760d14a4185fd112f1b0032eed790736
kmkkkuk <kmkkuk@gmail.com>
2017-12-06 14:12:16 +0100

    hero api call added

---
+++ code/src/components/hero.js
@@ -0,0 +1,26 @@
+import React from "react"
+
+class Hero extends React.Component {
+
+  componentDidMount() {
+    fetch("https://api.tictail.com/v1.26/stores/5znv").then((response) => {
+      return response.json()
+    }).then((json) => {
+    console.log(json)
+    this.setState({
+      store: json
+    })
+  })
+ }
+
+
+  render() {
+    console.log(this.state.wallpapers.iphone[0])
+    return (
+      <div className="hero">
+        <img src={this.state.wallpapers.iphone[0]} alt="" />
+      </div>
+    )
+  }
+}
+export default Hero
