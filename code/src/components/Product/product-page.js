b6af0e768db5543610c1238a307debbd5849877d
kmkkkuk <kmkkuk@gmail.com>
2017-12-11 16:23:14 +0100

    product-page changes

--- code/src/components/Product/product-page.js
+++ code/src/components/Product/product-page.js
@@ -4,19 +4,37 @@ import "./product.css"
 
 export default class ProductPage extends React.Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      products: []
+    }
+  }
+
+  componentDidMount() {
+    const productInfo = products.find(p => (p.prodId === this.props.match.params.id))
+    this.setProduct(product)
+  }
+
+  setProduct = (product) => {
+    this.setState({
+      product
+    })
+  }
+
   render() {
     return (
       <div className="ProductPage">
         <p>Breadcrumb path placeholder</p>
 
-        {<div className="imageHolder" style={{backgroundImage:`url(${this.props.prodImage})` }}>
+        {<div className="imageHolder" style={{backgroundImage:`url(${this.state.product.prodImage})` }}>
         </div>}
 
         <div className="product-info">
-          <h1>{this.props.prodName}
+          <h1>{this.state.product.prodName}
           </h1>
           <h2>Product details</h2>
-          <p>{this.props.prodDescription}</p>
+          <p>{this.state.product.prodDescription}</p>
         </div>
       </div>
     )
